417673622a91a8899f20a75a344b7fdb5c01d889
Jonas Raum <75501665+jonasraum@users.noreply.github.com>
2020-12-05 00:09:35 +0100

    day 01: optimizes first part to pointer method

--- 2020/Day 01 - Report Repair/Program.cs
+++ 2020/Day 01 - Report Repair/Program.cs
@@ -11,6 +11,7 @@ namespace Day_1___Report_Repair
             string[] inputString = File.ReadAllLines(filePath);
             int[] input = new int[inputString.Length];
             bool isNumber = false;
+            int leftPointer, rightPointer;
 
             for (int i = 0; i < inputString.Length; i++)
             {
@@ -24,60 +25,67 @@ namespace Day_1___Report_Repair
             Console.WriteLine("Day 01");
             Console.WriteLine("=======");
 
-            //Part 1
+            #region Part 1
 
             Console.WriteLine("\nPart 1:\n");
 
-            int firstNumber;
-            int secondNumber;
+            Array.Sort(input);
 
-            for (int i = 0; i < input.Length; i++)
+            leftPointer = 0;
+            rightPointer = input.Length - 1;
+
+            while (leftPointer < rightPointer)
             {
-                firstNumber = input[i];
-                for (int j = i + 1; j < input.Length; j++)
+                if (input[leftPointer] + input[rightPointer] == 2020)
                 {
-                    if (firstNumber + input[j] == 2020)
-                    {
-                        secondNumber = input[j];
-                        Console.WriteLine("Number 1: " + firstNumber);
-                        Console.WriteLine("Number 2: " + secondNumber);
-                        Console.WriteLine("Result: " + firstNumber * secondNumber);
-                    }
+                    Console.WriteLine("Number 1: " + input[leftPointer]);
+                    Console.WriteLine("Number 2: " + input[rightPointer]);
+                    Console.WriteLine("Result: " + input[leftPointer] * input[rightPointer]);
+                    break;
+                }
+                else if (input[leftPointer] + input[rightPointer] < 2020)
+                {
+                    leftPointer++;
+                }
+                else
+                {
+                    rightPointer--;
                 }
             }
 
-            //Part 2
+            #endregion
+
+            #region Part 2
 
             Console.WriteLine("\nPart 2:\n");
-
-            Array.Sort(input);
-
-            int l, r;
+            
             for (int i = 0; i < input.Length - 2; i++)
             {
-                l = i + 1;
-                r = input.Length - 1;
+                leftPointer = i + 1;
+                rightPointer = input.Length - 1;
 
-                while (l < r)
+                while (leftPointer < rightPointer)
                 {
-                    if (input[i] + input[l] + input[r] == 2020)
+                    if (input[i] + input[leftPointer] + input[rightPointer] == 2020)
                     {
                         Console.WriteLine("Number 1: " + input[i]);
-                        Console.WriteLine("Number 2: " + input[l]);
-                        Console.WriteLine("Number 3: " + input[r]);
-                        Console.WriteLine("Result: " + input[i] * input[l] * input[r]);
+                        Console.WriteLine("Number 2: " + input[leftPointer]);
+                        Console.WriteLine("Number 3: " + input[rightPointer]);
+                        Console.WriteLine("Result: " + input[i] * input[leftPointer] * input[rightPointer]);
                         break;
                     }
-                    else if(input[i] + input[l] + input[r] < 2020)
+                    else if(input[i] + input[leftPointer] + input[rightPointer] < 2020)
                     {
-                        l++;
+                        leftPointer++;
                     }
                     else
                     {
-                        r--;
+                        rightPointer--;
                     }
                 }
             }
+
+            #endregion
         }
     }
 }
