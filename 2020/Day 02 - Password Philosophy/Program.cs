1c8574fd5f4b152d581b7e354c8d674352abd72a
Jonas Raum <75501665+jonasraum@users.noreply.github.com>
2020-12-05 14:12:35 +0100

    Day 02: Comments added

--- 2020/Day 02 - Password Philosophy/Program.cs
+++ 2020/Day 02 - Password Philosophy/Program.cs
@@ -19,10 +19,11 @@ namespace Day_2___Password_Philosophy
             foreach (string s in passwords)
             {
                 int counter = 0;
-                string[] parts = s.Split(" ");
-                string[] range = parts[0].Split("-");
+                string[] parts = s.Split(" "); //creates an array containing the different arguments
+                string[] range = parts[0].Split("-"); //creates an array containing the minimum and maximum allowed occurence of the char
                 char regulatedChar = (parts[1])[0];
 
+                //checks how often the string contains the char
                 for (int i = 0; i < parts[2].Length; i++)
                 {
                     if ((parts[2])[i] == regulatedChar)
@@ -31,6 +32,7 @@ namespace Day_2___Password_Philosophy
                     }
                 }
 
+                //checks if the quantity complies to the regulations
                 if (Convert.ToInt32(range[0]) <= counter && counter <= Convert.ToInt32(range[1]))
                 {
                     correct++;
@@ -47,20 +49,23 @@ namespace Day_2___Password_Philosophy
             foreach (string s in passwords)
             {
                 bool a = false, b = false;
-                string[] parts = s.Split(" ");
-                string[] stringPositions = parts[0].Split("-");
+                string[] parts = s.Split(" "); //creates an array containing the different arguments
+                string[] stringPositions = parts[0].Split("-"); //creates an array containing the minimum and maximum allowed occurence of the char
                 char regulatedChar = (parts[1])[0];
 
+                //checks if the char occures at the first given position
                 if ((parts[2])[Convert.ToInt32(stringPositions[0]) - 1] == regulatedChar)
                 {
                     a = true;
                 }
 
+                //checks if the char occures at the second given position
                 if ((parts[2])[Convert.ToInt32(stringPositions[1]) - 1] == regulatedChar)
                 {
                     b = true;
                 }
 
+                //checks if the char only occures on one position
                 if (a != b)
                 {
                     correct++;
