329a3671ec93c82b11917e5a89633cc9a1933533
Jonas Raum <75501665+jonasraum@users.noreply.github.com>
2020-12-05 11:09:22 +0100

    Day 05: Added comments

--- 2020/Day 05 - Binary Boarding/Program.cs
+++ 2020/Day 05 - Binary Boarding/Program.cs
@@ -18,20 +18,25 @@ namespace Day_05___Binary_Boarding
 
             foreach (string seat in seats)
             {
+                //splits the string into the row and column group
                 string row = seat.Substring(0, seat.Length - 3);
                 string column = seat.Substring(seat.Length - 3);
 
+                //converts the char code into binary code
                 row = row.Replace('B', '1');
                 row = row.Replace('F', '0');
                 column = column.Replace('R', '1');
                 column = column.Replace('L', '0');
 
+                //converts the binary code into decimal integers
                 int rowNumber = Convert.ToInt32(row, 2);
                 int columnNumber = Convert.ToInt32(column, 2);
+
                 int seatID = rowNumber * 8 + columnNumber;
 
                 Console.WriteLine($"{seat}: row {rowNumber}, column {columnNumber}, seat ID {seatID}");
 
+                //checks if the current seatID is the highest as of yet
                 if (seatID > maxSeatID)
                 {
                     maxSeatID = seatID;
@@ -44,6 +49,8 @@ namespace Day_05___Binary_Boarding
             Array.Sort(seatIDs);
             int mySeatID = 0;
 
+            //searches for two elements in the sorted array, where a number was skipped between them
+            //if this is the case, the skipped number is our seat ID
             for (int i = 1; i < seatIDs.Length - 1; i++)
             {
                 if (seatIDs[i] == seatIDs[i-1] + 2)
