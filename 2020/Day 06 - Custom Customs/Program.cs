965f58385e2c21381cbb48cc1418aa3fd88ffda5
Jonas Raum <75501665+jonasraum@users.noreply.github.com>
2020-12-06 16:12:09 +0100

    Day 06: Comments added

--- 2020/Day 06 - Custom Customs/Program.cs
+++ 2020/Day 06 - Custom Customs/Program.cs
@@ -8,17 +8,14 @@ namespace Day_06___Custom_Customs
         static void Main(string[] args)
         {
             string filePath = @"D:\Jonas\Coding\Challanges\Advent of Code\2020\Day 06 - Custom Customs\Day 06 - Input.txt";
-            string lines = File.ReadAllText(filePath);
-            lines = lines.Replace("\r\n", " ");
-            string[] groups = lines.Split("  ");
+            string[] groups = File.ReadAllText(filePath).Replace("\r\n", " ").Split("  ");
             int sumDifferentAnswers = 0, sumSameAnswers = 0;
-            string possibleAnswers = "abcdefghijklmnopqrstuvwxyz";
+            string possibleStatements = "abcdefghijklmnopqrstuvwxyz";
 
-
-            foreach (string s in groups)
+            foreach (string group in groups)
             {
-                CountDifferentAnswers(s, possibleAnswers, ref sumDifferentAnswers);
-                CountSameAnswers(s, possibleAnswers, ref sumSameAnswers);
+                CountDifferentAnswers(group, possibleStatements, ref sumDifferentAnswers);
+                CountSameAnswers(group, possibleStatements, ref sumSameAnswers);
             }
 
             Console.WriteLine("Day 06");
@@ -26,41 +23,49 @@ namespace Day_06___Custom_Customs
 
             Console.Write("Part 1: ");
             Console.WriteLine(sumDifferentAnswers);
+            Console.Write("Part 2: ");
             Console.WriteLine(sumSameAnswers);
         }
 
-        private static void CountSameAnswers(string s, string possibleAnswers, ref int sumSameAnswers)
+        /// <summary>
+        /// Only counts the letters that are given by every person of the group
+        /// </summary>
+        /// <param name="group">the group of statements that has to be observed</param>
+        /// <param name="possibleStatements">all possible statements pooled together</param>
+        /// <param name="sumSameAnswers">the acummulated sum of all groups observed until now</param>
+        private static void CountSameAnswers(string group, string possibleStatements, ref int sumSameAnswers)
         {
-            string[] entries = s.Split(" ");
-            for (int i = 0; i < entries.Length; i++)
+            string[] entries = group.Split(" ");
+            for (int i = 0; i < entries.Length; i++) //checks every entry discrete
             {
-                for (int j = 0; j < possibleAnswers.Length; j++)
+                for (int j = 0; j < possibleStatements.Length; j++) //checks for every possible statement if it is included in the entry
                 {
-                    if (!entries[i].Contains(possibleAnswers[j]))
+                    if (!entries[i].Contains(possibleStatements[j]))
                     {
-                        possibleAnswers = possibleAnswers.Replace(possibleAnswers[j], '-');
+                        possibleStatements = possibleStatements.Replace(possibleStatements[j].ToString(), string.Empty);
+                        j--;
                     } 
                 }
             }
-            possibleAnswers = possibleAnswers.Replace("-", "");
-            sumSameAnswers += possibleAnswers.Length;
+            sumSameAnswers += possibleStatements.Length;
         }
 
-        static void CountDifferentAnswers(string s, string possibleAnswers, ref int sum)
+        /// <summary>
+        /// Counts the letters that are given by at least person of the group
+        /// </summary>
+        /// <param name="group">the group of statements that has to be observed</param>
+        /// <param name="possibleStatements">all possible statements pooled together</param>
+        /// <param name="sumDifferentAnswers">the acummulated sum of all groups observed until now</param>
+        static void CountDifferentAnswers(string group, string possibleStatements, ref int sumDifferentAnswers)
         {
-            int counter = 0;
-            for (int i = 0; i < s.Length; i++)
+            for (int i = 0; i < group.Length; i++) //checks for every letter in the group
             {
-                if (possibleAnswers.Contains(s[i]))
+                if (possibleStatements.Contains(group[i]))
                 {
-                    counter++;
-                    possibleAnswers = possibleAnswers.Replace(s[i], '-');
+                    possibleStatements = possibleStatements.Replace(group[i].ToString(), string.Empty); 
                 }
             }
-            sum += counter;
-            counter = 0;
+            sumDifferentAnswers += 26 - possibleStatements.Length;
         }
-
-      
     }
 }
